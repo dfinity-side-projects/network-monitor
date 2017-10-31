1f82e508a938ab8b4be47e4f75591b0f9844e05e
Derek Chiang <me@derekchiang.com>
2017-10-31 13:15:03 -0700

    Restructure routing

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/:metric', function(req, res, next) {
+  res.render('metric', { metric: req.params['metric'] });
 });
 
 module.exports = router;
