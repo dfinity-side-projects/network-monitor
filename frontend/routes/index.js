17a071e2dd772d6b489242e4ec117b18858f2d74
Derek Chiang <me@derekchiang.com>
2017-10-31 17:36:04 -0700

    Add an index with link to all metrics

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -2,6 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { metrics: {
+    'Average Block Latency': 'avg-block-latency',
+    'Block Propagation': 'block-propagation'
+  } });
+});
+
 router.get('/:metric', function(req, res, next) {
   res.render('metric', { metric: req.params['metric'] });
 });
